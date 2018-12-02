ce9af79778f8eb27e29da573f5d0737c99dac3dd
Сергей Белан <serg19-03@mail.ru>
2018-12-02 21:10:25 +0300

    tests

--- server/createRoom.js
+++ server/createRoom.js
@@ -1,68 +1,34 @@
-const createRoom = (io, roomTitle, serverName) => {
-  const roomNamespace = io.of(roomTitle);
+var redis = require('redis');
+
+const createRoom = (io, roomId, serverName) => {
+  const roomNamespace = io.of(`/${roomId}`);
+
+  var sub = null;
 
   roomNamespace.on('connection', function (socket) {
-    socket.emit('my-name-is', `${serverName}, room: ${roomTitle}`);
 
-    var addedUser = false;
-    var numUsers = 0;
+    if (!sub) {
+      sub = redis.createClient({host: 'redis', port: 6379});
+      sub.subscribe(`rooms/${roomId}`);
+    }
 
-    // when the client emits 'new message', this listens and executes
-    socket.on('new message', function (data) {
-      // we tell the client to execute 'new message'
-      socket.broadcast.emit('new message', {
-        username: socket.username,
-        message: data
-      });
-    });
+    socket.emit('my-name-is', `${serverName}, room: ${roomId}`);
 
-    // when the client emits 'add user', this listens and executes
-    socket.on('add user', function (username) {
-      if (addedUser) return;
-
-      // we store the username in the socket session for this client
-      socket.username = username;
-      ++numUsers;
-      addedUser = true;
-      socket.emit('login', {
-        numUsers: numUsers
-      });
-      // echo globally (all clients) that a person has connected
-      socket.broadcast.emit('user joined', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-    });
-
-    // when the client emits 'typing', we broadcast it to others
-    socket.on('typing', function () {
-      socket.broadcast.emit('typing', {
-        username: socket.username
-      });
-    });
-
-    // when the client emits 'stop typing', we broadcast it to others
-    socket.on('stop typing', function () {
-      socket.broadcast.emit('stop typing', {
-        username: socket.username
-      });
-    });
-
-    // when the user disconnects.. perform this
-    socket.on('disconnect', function () {
-      if (addedUser) {
-        --numUsers;
-
-        // echo globally that this client has left
-        socket.broadcast.emit('user left', {
-          username: socket.username,
-          numUsers: numUsers
+    sub.on('message', (chanell, msg) => {
+      let parsedMsg;
+      try {
+        parsedMsg = JSON.parse(msg);
+        socket.emit('new message', {
+          username: parsedMsg.user.name,
+          message: parsedMsg.text
         });
+      } catch (e) {
+        console.error(e);
       }
     });
-  });
 
-  return roomNamespace;
+    return roomNamespace;
+  })
 };
 
 module.exports = {
